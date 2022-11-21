c318d14d6631ce3cf471b692f4ae3e25756592c6
MyRainboww <76757647+MyRainboww@users.noreply.github.com>
2022-11-20 23:59:35 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -7174,9 +7174,11 @@ function asLipSync() {
         else if (bottomQueens.length == 2 && !disqOrDept || bottomQueens.length == 2 && currentCast.length == 4)
             bottomQueens[i].addToTrackRecord("BTM3");
         else if (disqOrDept && bottomQueens.length == 2 && !disqOrDeptFlag) {
+            disqOrDeptFlag = true;
             bottomQueens[i].addToTrackRecord(" BTM2");
         }
         else if (disqOrDept && bottomQueens.length == 3 && !disqOrDeptFlag) {
+            disqOrDeptFlag = true;
             bottomQueens[i].addToTrackRecord(" BTM3");
         }
         else

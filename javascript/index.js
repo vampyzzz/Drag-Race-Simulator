46805d94d44dcaf53daac87951cb73ce5c909ecc
vampyzzz <128661396+vampyzzz@users.noreply.github.com>
2023-04-17 12:14:32 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -8903,8 +8903,6 @@ let uk_season1 = [baga, blu, cheryl, crystaluk, divina, gothy, scaredy, sumting,
 let allstars_7 = [raja, jinkx, yvie, jaida, trinity, monet, shea, viv];
 // ALL STARS 8
 let allstars_8 = [alexism, darienne, heidi, jaymes, jessica, kahanna, kandym, kasha, lala, monica, naysha];
-// GLOBAL ALL STARS
-let allstars_32 = [alyssa, vanessa, peppermint, naomi, tia, bobo, jadashada, elektra, kweenkong, kana, turing, farida, tayce];
 
 //DRUK SEASON 2
 let awhora = new Queen("A'Whora", 7, 5, 8, 15, 10, 10, 8, "Awhora");
